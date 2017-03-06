5b941855b87c59fc351d6bf511390db739d0b070
Moses Gitau <gitaumoses4@gmail.com>
2017-03-06 20:18:14 +0300

    Removed more references to the csrf token in search

--- resources/views/widgets/popular_artists.blade.php
+++ resources/views/widgets/popular_artists.blade.php
@@ -3,11 +3,11 @@
         <img class="img-circle img-responsive" src="{{ $artist->image }}"/>
     </div>
     <div class="col-sm-7" style="text-align:left">
-        <a href="{{ URL::route("search",array("_token"=>csrf_token(),"query"=>$artist->name )) }}" class="music-title link" style="display: block">{{ $artist->name }}</a>
+        <a href="{{ URL::route("search",array("query"=>$artist->name )) }}" class="music-title link" style="display: block">{{ $artist->name }}</a>
         <span class="music-artist link link-light">View {{ $artist->name }}'s Songs</span>
     </div>
     <div class="col-sm-3">
-        <a href="{{ URL::route("search",array("_token"=>csrf_token(),"query"=>$artist->name )) }}">
+        <a href="{{ URL::route("search",array("query"=>$artist->name )) }}">
             <button class="btn btn-default btn-sm">More</button>
         </a>
     </div>
--- resources/views/widgets/recent_searches.blade.php
+++ resources/views/widgets/recent_searches.blade.php
@@ -1,5 +1,5 @@
 <div  style="float:left" class="small-margin">
-    <a href="{{ URL::route("search",array("_token"=>csrf_token(),"query"=>$recent->query)) }}">
+    <a href="{{ URL::route("search",array("query"=>$recent->query)) }}">
         <span class="label label-default recent-search">{{ $recent->query }}</span>
     </a>
 </div>
