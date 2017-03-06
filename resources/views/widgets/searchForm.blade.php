036d68fd13a839a5a009658ca6d9856e005ad303
Moses Gitau <gitaumoses4@gmail.com>
2017-03-06 20:13:05 +0300

    Removed csrf token from search

--- resources/views/widgets/searchForm.blade.php
+++ resources/views/widgets/searchForm.blade.php
@@ -4,7 +4,6 @@
         <h6 class="light">Download music for free</h6>
     </div>
     <form id="search-form" class="search-form" method="get" action="{{ URL::route("search") }}">
-        <input type="hidden" name="_token" value="{{ csrf_token() }} "/>
         <div class="input-group ui-widget">
             <input id="search-field" type="text" class="form-control autocomplete" name="query" placeholder="Search for mp3">
             <span type="submit" class="input-group-addon btn btn-primary" id="search-button"><i class="glyphicon glyphicon-search"></i></span>
